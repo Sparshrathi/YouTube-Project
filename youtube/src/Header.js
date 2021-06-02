e505cd6612185b38df7cd6ad8fd87bcb78b2b2c1
Sparshrathi <sparshrathi1999@gmail.com>
2021-06-02 15:04:03 +0530

    add Header

---
+++ youtube/src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from "@material-ui/core/Avatar";
+
+function Header() {
+    return (
+        <div className="header">
+
+        <div className="header_left">
+         
+        <MenuIcon />     
+        <img className="header_logo" 
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+        alt="" />
+        </div>
+
+        <div className="header_input">    
+        <input placeholder="Search" type="text"/>
+        <SearchIcon className="header_inputButton" />
+        </div>
+
+        <div className="header_icons">
+        <VideoCallIcon className="header_icon"/>
+        <AppsIcon className="header_icon"/>
+        <NotificationsIcon className="header_icon"/>
+        <Avatar alt="Sparsh Rathi"
+        src="https://avatars.githubusercontent.com/u/52618961?s=400&u=8a5555d9881c0caed7122cba4cdbc87339a2fdce&v=4"
+        />
+        </div>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ youtube/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCLIeQmoA9au7ZDjlSkDMAL_kSj0ifQvMo",
+    authDomain: "clone-a08ca.firebaseapp.com",
+    projectId: "clone-a08ca",
+    storageBucket: "clone-a08ca.appspot.com",
+    messagingSenderId: "513963212270",
+    appId: "1:513963212270:web:4855b085ce981ef02022ff",
+    measurementId: "G-ZDVF0GWM6V"
+  };
